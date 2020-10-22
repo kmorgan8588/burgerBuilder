df0e7fe5fe01e70563c441111ecbda360d29f449
Kyle Morgan <kmorgan8588@gmail.com>
2020-10-21 17:38:31 -0700

    hardcorde basic layout for application

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+
+const layout = (props) => {
+    return (
+        <Fragment>
+            <div>
+                Toolbar, SideDrawer, Backdrop
+        </div>
+            <main>
+                {props.children}
+            </main>
+        </Fragment>
+    )
+}
+
+export default layout;
