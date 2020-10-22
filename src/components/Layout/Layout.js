ae29f0f6551f654cb3d1b6d36e66bc9e0e50c3c6
Kyle Morgan <kmorgan8588@gmail.com>
2020-10-22 14:54:03 -0700

    add logic to have sidedrawer close when toggling backgroud

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,31 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import styles from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => {
-    return (
-        <Fragment>
-            <SideDrawer />
-            <Toolbar />
-            <main className={styles.Content}>
-                {props.children}
-            </main>
-        </Fragment>
-    )
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
+    sideDrawerClosedHandler = () => {
+        this.setState({
+            showSideDrawer: false
+        })
+    }
+    render() {
+
+        return (
+            <Fragment>
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    close={this.sideDrawerClosedHandler} />
+                <Toolbar />
+                <main className={styles.Content}>
+                    {this.props.children}
+                </main>
+            </Fragment>
+        )
+    }
 }
 
-export default layout;
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import Logo from '../../Logo/Logo';
 import styles from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = (props) => {
+    let attachedClasses = [styles.SideDrawer, styles.Close].join(' ');
+    if (props.open) {
+        attachedClasses = [styles.SideDrawer, styles.Open].join(' ');
+    }
     return (
-        <div className={styles.SideDrawer}>
-            <div className={styles.Logo}>
-                <Logo />
+        <Fragment>
+            <Backdrop show={props.open} remove={props.close} />
+            <div className={attachedClasses}>
+                <div className={styles.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Fragment>
     );
 }
 
