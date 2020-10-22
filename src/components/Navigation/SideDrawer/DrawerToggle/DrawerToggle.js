4a3cd1fc9ff00c5616f5c38e34717d5e5d406e83
Kyle Morgan <kmorgan8588@gmail.com>
2020-10-22 15:10:24 -0700

    adds menu styling to only display it when in Mobile, otherwise the logo takes over its position

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import styles from './DrawerToggle.module.css';
 
 const drawerToggle = (props) => {
     return (
-        <div onClick={props.toggle}>MENU</div>
+        <div className={styles.DrawerToggle} onClick={props.toggle}>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
     )
 }
 
