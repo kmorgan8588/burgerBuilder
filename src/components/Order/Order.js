c0322c1520c38c21ca86f982a1c104473f0dc9a2
Kyle Morgan <kmorgan8588@gmail.com>
2020-11-02 11:28:18 -0800

    fetches orders from the backend and displays them/their ingredients and price

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,10 +2,24 @@ import React from 'react';
 import styles from './Order.module.css';
 
 const order = (props) => {
+    const ingredients = Object.entries(props.ingredients)
+
+    const ingredientOutput = ingredients.map(pair => {
+        return (
+            <span style={{
+                textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                border: '1px solid #ccc',
+                padding: '5px'
+            }}
+                key={pair[0]}>{pair[0]} ({pair[1]})</span>
+        )
+    })
     return (
         <div className={styles.Order}>
-            <p>Ingredients: Lettuce (1)</p>
-            <p> Price: <strong>USD 5.45</strong></p>
+            <p>Ingredients: {ingredientOutput}</p>
+            <p> Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
         </div>
     );
 }
